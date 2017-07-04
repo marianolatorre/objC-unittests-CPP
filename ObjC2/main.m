845b15f063ca8a15e055700e5a7a827ba8db77b9
mariano latorre <marianolatorre@gmail.com>
2017-07-04 20:07:40 +0100

    Initial Commit

---
+++ ObjC2/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ObjC2
+//
+//  Created by mariano latorre on 04/07/2017.
+//  Copyright © 2017 mariano latorre. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
